d6086c15fa55b09876e4d8b6ecd1e411ff2d37e2
Brandon VanderBeek <brandon.p.vanderbeek@gmail.com>
2025-05-15 08:49:42 +0100

    Revised script comments

--- examples/scripts/julia/call_psi_s.jl
+++ examples/scripts/julia/call_psi_s.jl
@@ -3,13 +3,17 @@
 ###################################
 # Julia script to run rj-mcmc inversion via the shell call,
 # >> julia call_psi_s.jl parameter_file num_chains_per_chunk num_chunk
-# To use multi-threading when ray-tracing,
+# Can also call with --check-bounds=no flag for potenital performance gain
+# To use multi-threading when 3D ray-tracing,
 # >> julia --threads 28 call_psi_s.jl parameter_file num_chains_per_chunk num_chunk
-# Can also call with --check-bounds=no flag
 # Where parameter_file is a TOML-file specifying the PSI_S inversion parameters, num_chains_per_chunk is the
 # number of Markov chains to run in parallel (must match available resources) and num_chunk specifies which
 # batch of chains we are running. For example, if we want to run a total of 56 chains on two independent
 # compute nodes (i.e. different jobs) with 28 cores each, we would need to run 2 chunks of 28 Markov chains each.
+# Note! JavaCall, which is required for using TauP, breaks mulit-threading in Julia. This appears to be the result
+# of defining the environment variable JULIA_COPY_STACKS = 1 (https://github.com/JuliaLang/julia/issues/44589).
+# For this reason, TauP is no longer loaded via dependencies.jl. If needed for teleseismic inversions, load the
+# package as part of this script. TauP does not use any parallel functionality.
 #
 # Results are saved to the output location defined in parameter_file. Following files are written:
 # 1) parameters.toml -- copy of parameter used to run inversion
@@ -18,9 +22,14 @@
 # 4) psi_s_utilities.jld
 
 # Set-Up Environment Variables #
-# Not required if defined in job submission script. 
+# Alternatively, define in HPC job submission script. 
 # The "TAUP_JAR" variable is optional (default is that which ships with TauP.jl)
-ENV["TAUP_JAR"] = "/Users/bvanderbeek/research/software/TauP_Toolkit/TauP-2.7.0-SNAPSHOT5/lib/TauP-2.7.0-SNAPSHOT5.jar"
+# The following three lines are only needed if using TauP to trace rays (e.g. teleseismic imgaing)
+# If not using TauP, these lines should not be active as they will break parallel 3D ray tracing capabilities
+# ENV["TAUP_JAR"] = "TauP-2.7.0-SNAPSHOT5/lib/TauP-2.7.0-SNAPSHOT5.jar"
+# ENV["JULIA_COPY_STACKS"] = "yes" # For non-windows users, this environment variable may be required for JavaCall to function properly; may break multi-threading
+# using TauP
+# PSI_S source code directory
 ENV["PSI_S"] = "/Users/bvanderbeek/research/software/GitRepos/PSI_S"
 
 # Load Required Packages #
